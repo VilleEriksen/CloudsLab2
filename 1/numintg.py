1204401d6674338b15ae21fde1e119d712b44c73
Ville Eriksen <ville.eriksen@gmail.com>
2024-12-14 21:22:58 +0100

    Task 1 done

---
+++ 1/numintg.py
@@ -0,0 +1,25 @@
+from math import *
+
+def intgcossin(lower, upper, n):
+    sum = 0.0
+    h = (upper - lower) / n
+    
+    for i in range(n):
+        x = lower + (i * h)
+        
+        y = abs(sin(x))
+        sum += h * y
+    
+    return sum
+        
+if __name__ == "__main__":
+    while True:
+        try:
+            lower = float(input("Lower: "))
+            upper = float(input("Upper: "))
+            n = int(input("Intervals: "))
+            
+            result = intgcossin(lower, upper, n)
+            print(f"{result:.5f}")
+        except ValueError:
+            print("ValueError")
