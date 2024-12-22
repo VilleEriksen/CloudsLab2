6dfcc11ad26332b6a789cb40e7c160e0b5479295
Ville Eriksen <ville.eriksen@gmail.com>
2024-12-22 16:27:40 +0100

    Update to task 3

--- 2/app.py
+++ 2/app.py
@@ -3,8 +3,22 @@ from math import *
 
 app = Flask(__name__)
 
-@app.route('/calc/<lower>/<upper>/<n>')
-def flask_intgabssin(lower, upper, n):
+@app.route('/calc/<lower>/<upper>')
+def flask_intgabssin(lower, upper):
+    lower = float(lower)
+    upper = float(upper)
+    
+    n10 = calc(lower, upper, 10)
+    n100 = calc(lower, upper, 100)
+    n1000 = calc(lower, upper, 1000)
+    n10k = calc(lower, upper, 10000)
+    n100k = calc(lower, upper, 100000)
+    n1m = calc(lower, upper, 1000000)
+    
+    return f"10: {n10} | 100: {n100} | 1000: {n1000} | 10k: {n10k} | 100k: {n100k} | 1m: {n1m}"
+    
+
+def calc(lower, upper, n):
     lower = float(lower)
     upper = float(upper)
     n = int(n)
--- 3/locustfile.py
+++ 3/locustfile.py
@@ -3,4 +3,4 @@ from locust import HttpUser, task, between
 class CalcUser(HttpUser):
     @task
     def hello_world(self):
-        self.client.get("/calc/3/3.14159/100")
+        self.client.get("/calc/0/3.14159")
